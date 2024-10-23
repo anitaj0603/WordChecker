0544d36ae16dfa01d842da45e0178cf3a85a53c6
anitaj0603 <anitaj10@nycstudents.net>
2024-10-23 13:28:59 +0000

    Finsihed Part A

--- Main.java
+++ Main.java
@@ -1,13 +1,21 @@
 import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
-        WordChecker w =  new WordChecker ();
         ArrayList<String> words = new ArrayList<String>();
         words.add("an");
         words.add("band");
         words.add("band");
         words.add("abandon");
-        WordChecker x = new WordChecker(words);
+        WordChecker w =  new WordChecker (words);
         System.out.println(words);
+        System.out.println(w.isWordChain());
+        ArrayList<String> words1 = new ArrayList<String>();
+        words1.add("to");
+        words1.add("too");
+        words1.add("stool");
+        words1.add("tools");
+        WordChecker x = new WordChecker(words1);
+        System.out.println(words1);
+        System.out.println(x.isWordChain());
     }
 }
--- WordChecker.java
+++ WordChecker.java
@@ -11,11 +11,13 @@ public class WordChecker {
      * Postcondition: wordList is unchanged.
      */
     public boolean isWordChain() {
-        for(int i = 1; i < wordList.size(); i++)
         boolean wordChain = true;
-        String after = wordList.get(i);
-        String before = wordList.get(i - 1);
-        if(after.indexOf(before) < 0) return false;
+        for(int i = 1; i < wordList.size(); i++) {
+            String after = wordList.get(i);
+            String before = wordList.get(i - 1);
+            if(after.indexOf(before) < 0) return false;
+        }
+        return wordChain;
 
         /* to be implemented in part (a) */ }
 
@@ -29,7 +31,13 @@ public class WordChecker {
      * wordList.
      */
     public ArrayList<String> createList(String target) {
-        /* to be implemented in part (b) */ }
+        ArrayList<String> list = new ArrayList<String>();
+        for(String s : wordList){
+            if(s.indexOf(target) == 0 );
+            list.add(s.substring(target.length()));
+        }
+        return list;
+    }
     public WordChecker(ArrayList<String> list) {
         wordList = list;
     }

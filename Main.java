fd8092dce78fd6214e8643752c23e2c4590c368e
anitaj0603 <anitaj10@nycstudents.net>
2024-10-24 13:29:11 +0000

    Finished code. Caught error.

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
+        
         ArrayList<String> words = new ArrayList<String>();
         words.add("an");
         words.add("band");
@@ -9,6 +10,7 @@ public class Main {
         WordChecker w =  new WordChecker (words);
         System.out.println(words);
         System.out.println(w.isWordChain());
+
         ArrayList<String> words1 = new ArrayList<String>();
         words1.add("to");
         words1.add("too");
@@ -17,5 +19,18 @@ public class Main {
         WordChecker x = new WordChecker(words1);
         System.out.println(words1);
         System.out.println(x.isWordChain());
+
+        ArrayList<String> words2 = new ArrayList<String>();
+        words2.add("catch");
+        words2.add("bobcat");
+        words2.add("catchacat");
+        words2.add("cat");
+        words2.add("at");
+        WordChecker a = new WordChecker(words2);
+        System.out.println(words2);
+        System.out.println(a.createList("cat"));
+        System.out.println(a.createList("catch"));
+        System.out.println(a.createList("dog"));
+
     }
 }
--- WordChecker.java
+++ WordChecker.java
@@ -1,15 +1,8 @@
 import java.util.ArrayList;
 public class WordChecker {
-    /** Initialized in the constructor and contains no null elements */
+
     private ArrayList<String> wordList;
 
-    /**
-     * Returns true if each element of wordList (except the first) contains the
-     * previous
-     * element as a substring and returns false otherwise, as described in part (a)
-     * Precondition: wordList contains at least two elements.
-     * Postcondition: wordList is unchanged.
-     */
     public boolean isWordChain() {
         boolean wordChain = true;
         for(int i = 1; i < wordList.size(); i++) {
@@ -18,22 +11,12 @@ public class WordChecker {
             if(after.indexOf(before) < 0) return false;
         }
         return wordChain;
+    }
 
-        /* to be implemented in part (a) */ }
-
-    /**
-     * Returns an ArrayList<String> based on strings from wordList that start
-     * with target, as described in part (b). Each element of the returned ArrayList
-     * has had
-     * the initial occurrence of target removed.
-     * Postconditions: wordList is unchanged.
-     * Items appear in the returned list in the same order as they appear in
-     * wordList.
-     */
     public ArrayList<String> createList(String target) {
         ArrayList<String> list = new ArrayList<String>();
         for(String s : wordList){
-            if(s.indexOf(target) == 0 );
+            if(s.indexOf(target) == 0 )
             list.add(s.substring(target.length()));
         }
         return list;
@@ -45,5 +28,4 @@ public class WordChecker {
         wordList = new ArrayList<String>();
 
     }
-    
 }
